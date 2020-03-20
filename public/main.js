38c50dd1dc5526a90b951f560895eb2a2135d7ae
Stefan Schmidhuber <stefan@jupiter.fritz.box>
2020-03-20 23:19:30 +0100

    display time span to last data update in UI

--- public/main.js
+++ public/main.js
@@ -1,13 +1,39 @@
 $( document ).ready(function() {
-  console.log('ready');
-  $.get('/datatable', function(data){
-    $('#spinner').css('visibility','hidden');
-    $('#stockdata').html(data);
-    $('#dataframe').DataTable();
+
+  // load initial securities table
+  $.get('/securities', function(res){
+    //$('#spinner').css('visibility','hidden');
+    $('#spinner').addClass('invisible')
+    $('#stockdata').html(res)
+    $('#dataframe').DataTable()
   });
 
+  // display time since last data update
+  $.get('/securities/metadata', function(res){
+    interval = res.interval
+    lastupdate = Date.parse(res.lastupdate)
+
+    function displayTime() {
+      let time_string;
+      time = Math.round((Date.now() - lastupdate) / 1000 / 60)
+      hours = Math.floor(time / 60)
+      minutes = time % 60
+      if (hours == 0) {
+        time_string = minutes + " min"
+      } else {
+        time_string = hours + " h " + minutes + " min"
+      }
+      $('#lastupdate').text(time_string)
+    }
+
+    displayTime()
+    $('#metadata').removeClass('invisible')
+
+    setInterval(displayTime, 10000);
+  });
+
+  // navigation
   $('.nav-link').on('click', function(){
-    console.log("navigation")
     $('.top-level').hide()
     old = $('.nav-link.text-white')
     old.addClass('text-muted')
