97b792bac24bc842c2f82d5e3db93ba6cfe68d1e
Stefan Schmidhuber <stefan@jupiter.fritz.box>
2020-03-28 22:02:40 +0100

    implement filter logic in backend, extend securities endpoint to handle filter parameter

--- App.jl
+++ App.jl
@@ -10,6 +10,7 @@ using JSON
 using StringBuilders
 using Formatting
 using Printf
+using Statistics
 using Dates
 using UUIDs
 using .DataUpdate
@@ -30,10 +31,20 @@ SecurityFilter(dict::Dict) = SecurityFilter(uuid1(),dict)
 
 # GET /securities
 function get_securities(c::AppController)
+    filter_id = haskey(c.params, "filter") ? filter_id = c.params["filter"] : nothing
     db = SQLite.DB(dbfile)
     securities = DBInterface.execute(db, "SELECT * FROM Securities") |> DataFrame
     updates = DBInterface.execute(db, "SELECT timestamp FROM Updates ORDER BY timestamp DESC LIMIT 1") |> DataFrame
+    if filter_id != nothing
+        rs = DBInterface.execute(db, "SELECT filter FROM SecurityFilters WHERE id = ?", (filter_id,)) |> DataFrame
+        filter = SecurityFilter(filter_id |> UUID, JSON.Parser.parse(rs.filter |> first))
+    end
     DBInterface.close!(db)
+
+    if filter_id != nothing
+        apply!(securities, filter)
+    end
+
     lastupdate = updates.timestamp |> first
     res = Dict()
     res["rows"] = preparedata(securities)
@@ -42,6 +53,8 @@ function get_securities(c::AppController)
     vals = Dict()
     vals["revenue"] = [Int64(round(securities.revenue |> skipmissing |> minimum, digits=0)), Int64(round(securities.revenue |> skipmissing |> maximum, digits=0))]
     vals["incomeNet"] = [Int64(round(securities.incomeNet |> skipmissing |> minimum, digits=0)), Int64(round(securities.incomeNet |> skipmissing |> maximum, digits=0))]
+    vals["priceEarningsRatio"] = [round(securities.priceEarningsRatio |> skipmissing |> minimum, digits=2), round(securities.priceEarningsRatio |> skipmissing |> maximum, digits=2)]
+    vals["priceBookRatio"] = [round(securities.priceBookRatio |> skipmissing |> minimum, digits=2), round(securities.priceBookRatio |> skipmissing |> maximum, digits=2)]
     vals["industry"] = securities.industry |> skipmissing |> unique |> sort
     vals["sector"] = securities.sector |> skipmissing |> unique |> sort
     vals["subsector"] = securities.subsector |> skipmissing |> unique |> sort
@@ -97,9 +110,55 @@ function preparedata(dataframe::DataFrame)
     end
 end
 
+# apply security filter to security data frame
+function apply!(securities::DataFrame, securityfilter::SecurityFilter)
+    filter = securityfilter.filter
+
+    # filter categories
+    mapping_categorical = Dict("country" => :country)
+    for (k,v) in mapping_categorical
+        if haskey(filter, k)
+            filter!(row -> row[v] in filter[k], securities)
+        end
+    end
+
+    # filter intervals
+    mapping_intervals = Dict("revenue" => :revenue, "incomeNet" => :incomeNet, "priceEarningsRatio" => :priceEarningsRatio, "priceBookRatio" => :priceBookRatio)
+    for (k,v) in mapping_intervals
+        if haskey(filter, k)
+            filter!(row -> row[v] >= filter[k][1] && row[v] <= filter[k][2], securities)
+        end
+    end
+
+
+    dfcopy = copy(securities)   # this is used to calculate the quantile of different values
+
+    # get lower than quantile
+    mapping_percentiles = Dict("p-per" => :priceEarningsRatio, "p-pbr" => :priceBookRatio)
+    for (k,v) in mapping_percentiles
+        if haskey(filter, k)
+            threshold = quantile(dfcopy[!,v] |> skipmissing, filter[k])
+            filter!(row -> row[v] !== missing ? row[v] <= threshold : false, securities)
+        end
+    end
+
+    # get higher than quantile
+    mapping_percentiles = Dict("p-drrl" => :dividendReturnRatioLast, "p-drr3" => :dividendReturnRatioAvg3, "p-drr5" => :dividendReturnRatioAvg5)
+    for (k,v) in mapping_percentiles
+        if haskey(filter, k)
+            threshold = quantile(dfcopy[!,v] |> skipmissing, filter[k])
+            filter!(row -> row[v] !== missing ? row[v] >= threshold : false, securities)
+        end
+    end
+end
+
+
 # security filter validation
 function isvalid(securityfilter::SecurityFilter)::Bool
     dict = securityfilter.filter
+    if keys(dict) |> length == 0
+        return false
+    end
 
     intervals = ["revenue", "incomeNet"]
     interval_check = map(intervals) do i
