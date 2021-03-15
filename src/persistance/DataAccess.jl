428688df0ae677d6117eb306874067c8931504da
Stefan Schmidhuber <stefan.schmidhuber@gmx.at>
2021-03-15 20:56:32 +0100

    get_exchange_rates() implemented

--- src/persistance/DataAccess.jl
+++ src/persistance/DataAccess.jl
@@ -5,11 +5,11 @@ using DataFrames
 using Dates
 using ..Model
 
-DATA_BASE = "data/stockoverview.sqlite"
+DB = "data/stockoverview.sqlite"
 
 # store the given exchange rates to DB if they don't exist already
 function store_exchange_rates(exchange_rates::EuroExchangeRates)
-    db = SQLite.DB(DATA_BASE)
+    db = SQLite.DB(DB)
     n = DBInterface.execute(db, "select * from euro_exchange_rates where date = $(exchange_rates.date);") |> DataFrame |> nrow
     if n != 0
         stmt = SQLite.Stmt(db, "INSERT INTO euro_exchange_rates (date, currency, rate) VALUES (?,?,?);")
@@ -20,9 +20,22 @@ function store_exchange_rates(exchange_rates::EuroExchangeRates)
     end
 end
 
-# retrieve exchange rates closest to the given date
-function get_exchange_rates(date::Date)::EuroExchangeRates
-    @warn "not implemented yet"
+# returns latest available euro exchange rates
+function get_exchange_rates()::Union{EuroExchangeRates,Nothing}
+    db = SQLite.DB(DB)
+    rs = DBInterface.execute(db, "select * from euro_exchange_rates where date = (select max(date) from euro_exchange_rates);") |> DataFrame
+
+    if nrow(rs) == 0
+        # not exchange rates available
+        return nothing
+    end
+
+    rates = Dict()
+    for i in 1:length(rs.currency)
+        rates[rs.currency[i]] = rs.rate[i]
+    end
+
+    return EuroExchangeRates(rs.date |> first |> Date, rates)
 end
 
 end # module
