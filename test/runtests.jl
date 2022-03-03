c8c9c6ed47d00fbd47b45d4d026741de8fb9a617
Stefan Schmidhuber <stefan.schmidhuber@gmx.at>
2022-03-03 23:40:49 +0100

    add tests for data ingestion

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,8 +1,19 @@
 using StockOverview
 using Test
+using Dates
 
 
-@testset "Data Access" begin
-    @test 1 + 1 == 2
-    @test 2 == 2
+@testset "Data Ingestion" begin
+    security = DataIngestion.fetchsecurityheader("DE0008404005")
+    @test security isa Model.Security
+    @test security.name == "Allianz"
+    security = DataIngestion.fetchsecurityheader("vinvalid ISIN")
+    @test security isa Model.Security
+    @test security.name === missing
+
+    exchangerates = DataIngestion.fetchexchangerates()
+    @test exchangerates isa Model.EuroExchangeRates
+    @test exchangerates.date == today()
+    @test exchangerates.rates["JPY"] > 1
+    @test exchangerates.rates["GBP"] < 1
 end
